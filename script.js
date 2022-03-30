8b16a89fb8e0547d61b4a8948b16a5771230c06f
medblutex <47444914+medblutex@users.noreply.github.com>
2022-03-30 16:08:46 +0200

    Complete The Project

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 let choices = document.querySelectorAll('ul li');
-choices.forEach(choice => choice.addEventListener('click', selected))
-function selected () {
-    choiceArray = Array.from(choices);
-    for (const item of choiceArray) {
-        console.log(item);
+let rate = 0;
+const submit = document.querySelector('button');
+const ratingContainer = document.querySelector('.wrapper');
+const resultContainer = document.querySelector('.result-wrapper');
+const ratingNum = document.querySelectorAll('.rating-num');
+const selection = document.querySelector('.selection');
+ratingNum.forEach(rate => rate.addEventListener('click', selected))
+function selected (e) {
+    e.preventDefault();
+    ratingArray = Array.from(ratingNum);
+    for (const item of ratingArray) {
         if (item.classList.contains('selected')) {
             item.classList.remove('selected');
         }
     }
-    this.classList.toggle("selected");
+    if(e.target.classList = 'rating-num') {
+        e.target.classList.add('selected');
+        rate = e.target.innerHTML;
+        selection.innerHTML = rate;
+        submit.addEventListener('click', thankYou);
+    } 
+    
+}
+submit.addEventListener('click', thankYou);
+function thankYou () {
+    ratingNum.forEach(rate => {
+        if (rate.classList.contains('selected')) {
+            resultContainer.classList.remove('visible');
+            ratingContainer.classList.add('visible');
+        } 
+    })
+    warning()
+}
+function warning () {
+    const warning = document.createElement('div');
+    warning.textContent="You need to choose 1 to 5 rating."
+    warning.style.color='red';
+    warning.style.marginTop='20px';
+    ratingContainer.appendChild(warning);
+    submit.removeEventListener('click', thankYou);  
+    
 }
